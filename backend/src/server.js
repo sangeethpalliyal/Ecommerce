4bb2f302c94578d625b5c14a897112b31f08b282
sangeethpalliyal <sangeethpalliyal1357@gmail.com>
2026-06-18 12:54:06 +0530

    feat: initialize backend server, setup mobile environment with Clerk, React Query, and Tailwind, and define core data types

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,15 @@
 import express from "express";
-
+import path from "path"
+import { ENV } from "./config/env.js";
 const app = express()
+
+const _dirname = path.resolve();
+
 app.get("/api/health",(res,req) => {
     res.status(200).json({message:"Success"});
 })
-app.listen(3000,() => console.log("Server is up and running"));
+
+if(ENV.NODE_ENV === "production"){
+    app.use(express.static(path.join(_dirname,"../admin/dist")))
+}
+app.listen(ENV.PORT,() => console.log("Server is up and running"));
--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,5 +1,22 @@
 import { Stack } from "expo-router";
 import "./global.css"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ClerkProvider } from '@clerk/expo'
+import { tokenCache } from '@clerk/expo/token-cache'
+
+const queryClient = new QueryClient();
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+if (!publishableKey) {
+  throw new Error('Add your Clerk Publishable Key to the .env file')
+}
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+    <QueryClientProvider client={queryClient}>
+      <Stack
+        screenOptions={{ headerShown: false }}
+      />
+    </QueryClientProvider>
+    </ClerkProvider>
+  );
 }
